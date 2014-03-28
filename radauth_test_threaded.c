781065836f6af05c26914fd56bf06ca9c1648b53
Julius Plenz <plenz@cis.fu-berlin.de>
2014-03-28 13:42:49 +0100

    radauth_test_threaded: make number of threads configurable via -n

--- radauth_test_threaded.c
+++ radauth_test_threaded.c
@@ -1,8 +1,10 @@
 #include <stdio.h>
 #include <string.h>
 #include <unistd.h>
+#include <stdlib.h>
 #include <pthread.h>
 #include <signal.h>
+#include <errno.h>
 #include "libradauth.h"
 
 struct autharg {
@@ -10,13 +12,12 @@ struct autharg {
 	char *vp;
 };
 
-#define MAXTHREADS 32
+static int numthreads = 32;
+static pthread_t *threads;
 
 static int running = 1;
 static struct sigaction sigint;
 static pthread_mutex_t wait_sigint = PTHREAD_MUTEX_INITIALIZER;
-
-static pthread_t threads[MAXTHREADS];
 
 /* signal handler */
 void sigint_called() {
@@ -45,6 +46,13 @@ int main(int argc, char *argv[])
 	struct autharg arg;
 	char vp[32 + 22];
 
+	if(argc >= 3 && !strcmp(argv[1], "-n")) {
+		numthreads = atoi(argv[2]);
+		fprintf(stdout, "Using %d thread%s.\n", numthreads, numthreads > 1 ? "s" : "");
+		argc -= 2;
+		argv += 2;
+	}
+
 	if(argc != 3) {
 		fprintf(stdout, "<user> <pw>: ");
 		fscanf(stdin, "%31s %31s", arg.username, arg.password);
@@ -64,17 +72,29 @@ int main(int argc, char *argv[])
 	sigint.sa_handler = sigint_called;
 	sigaction(SIGINT, &sigint, NULL);
 
+	threads = malloc(numthreads * sizeof(pthread_t));
+	if(!threads) {
+		fprintf(stderr, "Failed to allocate memory for threads!\n");
+		exit(-1);
+	}
+
 	/* dispatch threads */
-	for(i = 0; i < MAXTHREADS; i++)
-		pthread_create(&threads[i], NULL, auth, &arg);
+	for(i = 0; i < numthreads; i++) {
+		if(pthread_create(&threads[i], NULL, auth, &arg) == EAGAIN) {
+			fprintf(stderr, "Error spawning thread %d "
+				"(insufficient resources)\n", i);
+			numthreads = i;
+		}
+	}
 
 	/* waiting for sigint */
 	pthread_mutex_lock(&wait_sigint);
 	running = 0;
 	fprintf(stderr, "\nCtrl-C caught, waiting for threads to finish...\n");
 
-	for(i = 0; i < MAXTHREADS; i++)
+	for(i = 0; i < numthreads; i++)
 		pthread_join(threads[i], NULL);
+	free(threads);
 
 	fprintf(stderr, "\nAll done. Exiting.\n");
 
